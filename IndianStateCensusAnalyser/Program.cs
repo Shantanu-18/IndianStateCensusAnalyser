e511f51bf39f2dd6a6e5e4073c7e907b400702b5
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-09 23:36:56 +0530

    [Shantanu] Added : Main method and printing Welcome message.

--- IndianStateCensusAnalyser/Program.cs
+++ IndianStateCensusAnalyser/Program.cs
@@ -6,7 +6,7 @@ namespace IndianStateCensusAnalyser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Welcome to Indian State Census Analyser!");
+            Console.WriteLine("Hello Welcome to Indian State Census Analyser !");
         }
     }
 }
